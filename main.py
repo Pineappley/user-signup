662614293f61a09fd722357e85120913e5a99179
pineappley <zh.caro@gmail.com>
2017-01-30 19:31:15 -0600

    wrote an error message!

--- main.py
+++ main.py
@@ -15,50 +15,63 @@
 # limitations under the License.
 #
 import webapp2
+import cgi
+import re
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return USER_RE.match(username)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        header = "<h1>Signup</h1>"
-        screenname_form = """
-            <form>
-                <label>
-                    Username
-                    <input type="text" name="username"/>
-                </label>
-            </form>
+        header = """
+        <h1>Signup</h1>
+        <form method = 'post'>
+            <label>Username: <input name="username" /> </label>
+            <label>Password: <input type="password" name="password"/></label>
+            <label>Verify Password: <input type="password" name="password"/></label>
+            <label>Email: <input type="text" name="email"/> </label>
+        <input type="submit" name="Submit"/>
+        </form>
             """
-        password_form = """
-            <form>
-                <label>
-                    Password
-                    <input type="password" name="password"/>
-                </label>
-            </form>
-            """
-        verify_passform = """
-            <form>
-                <label>
-                    Verify Password
-                    <input type="password" name="password"/>
-                </label>
-            </form>
-            """
-        email_form = """
-            <form>
-                <label>
-                    Email
-                    <input type="text" name="email"/>
-                </label>
-            </form>
-        """
-        sub_button = """
-            <input type="submit" value="Submit"/>
-            """
-    
+        self.response.write(header)
 
-        allforms = screenname_form + password_form + verify_passform + email_form
-        self.response.write(header + allforms + sub_button)
+
+    def post(self):
+        header = """
+        <h1>Signup</h1>
+        <form method = 'post'>
+            <label>Username: <input name="username" /> </label></br>
+            <label>Password: <input type="password" name="password"/></label></br>
+            <label>Verify Password: <input type="password" name="password"/></label></br>
+            <label>Email: <input type="text" name="email"/> </label></br>
+        <input type="submit" name="Submit"/>
+        </form>
+            """
+        username = self.request.get("username")
+        if valid_username(username) == None:
+            error = "This is not a valid username."
+            error_element = "<p class='error'>" + cgi.escape(error, quote=True) + "</p>"
+            self.response.write(header + error_element)
+        else:
+            self.redirect("/here")
+
+
+
+        # username = self.request.get("username")
+        # for i in username:
+        #     if i == " ":
+        #         error = "Please enter a valid username"
+        #         error_escaped = cgi.escape(error, quote=True)
+        #         # redirect to homepage, and include error as a query parameter in the URL
+        #         self.redirect("/?error=" + error_escaped)
+
+class Welcome(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("Welcome to my temporary success celebration page!")
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    #('/validate', Validate)
 ], debug=True)
